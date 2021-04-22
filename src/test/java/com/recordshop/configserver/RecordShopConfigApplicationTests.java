30c53eb58e90b22dd14961e45be344e2057aad18
Matija Petrovic <matijapetrovicweb@gmail.com>
2021-04-22 16:14:59 +0200

    Initial config server setup

---
+++ src/test/java/com/recordshop/configserver/RecordShopConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recordshop.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
